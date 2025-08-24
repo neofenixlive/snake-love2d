c43429402c45cab51fb8137040d9d3664fc52b46
neofenixlive <neofenixlive@gmail.com>
2025-08-24 15:39:49 -0300

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 function love.conf(t)
     t.window.title = "SnakeLUA"
-    t.window.width = 900
-    t.window.height = 600
+    t.window.width = 640
+    t.window.height = 480
     t.window.fullscreen = false
 end
+
